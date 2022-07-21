225590a755a1d68bcbb1ac65c2f623966be7c5f0
JhonVB <jhonba2020@gmail.com>
2022-07-20 20:48:02 -0500

    CRUD completo

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -3,6 +3,7 @@ import {
   CREATE_PERSONA,
   DELETE_PERSONA,
   UPDATE_PERSONA,
+  CREATE_PERSONA_ERROR,
 } from "../Redux/actionsTypes";
 
 import axios from "../Service/Service";
@@ -17,19 +18,22 @@ export function getPersonas() {
   };
 }
 
-export function createPersona(persona) {
+export function createPersona(persona, clear) {
   return async function (dispatch) {
     try {
       const personas = await axios.post(`/person/`, persona);
+      console.log("post", personas);
+      clear();
       return dispatch({
         type: CREATE_PERSONA,
         payload: personas.data,
       });
     } catch (error) {
-      const errors = error.response.data;
+      console.log("error de post", error);
+      const errors = error.response?.data;
       return dispatch({
-        type: CREATE_PERSONA,
-        payload: { isError: true, errors },
+        type: CREATE_PERSONA_ERROR,
+        payload: errors,
       });
     }
   };
@@ -40,8 +44,6 @@ export function eliminarPersona(id) {
     await axios.delete(`/person/${id}/`);
     const personas = await axios.get(`/person/`);
 
-    console.log(personas);
-    //  console.log(personas.data);
     return dispatch({
       type: DELETE_PERSONA,
       payload: personas.data,
@@ -51,10 +53,7 @@ export function eliminarPersona(id) {
 
 export function actualizarPersona(id, info) {
   return async function (dispatch) {
-    const ver = await axios.put(`/person/${id}/`, info);
-    const personas = await axios.get(`/person/`);
-
-    console.log("ver", ver);
+    await axios.put(`/person/${id}/`, info);
 
     return dispatch({
       type: UPDATE_PERSONA,

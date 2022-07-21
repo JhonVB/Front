93b1d1a21577a698db1c68959a4f1ee19da79063
JhonVB <jhonba2020@gmail.com>
2022-07-21 09:45:32 -0500

    Algo de estilos

--- src/Page/Formulario/Formulario.js
+++ src/Page/Formulario/Formulario.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { Button, Form, Input, Select, Divider } from "antd";
+import { Button, Form, Input, Select, Divider, Row, Col } from "antd";
 import { createPersona } from "../../Redux/actions";
 import "./Formulario.css";
 import "antd/dist/antd.css";
@@ -38,79 +38,108 @@ function Formulario() {
 
   return (
     <div className="contendedor">
-      <Link to="/home">
-        <Button>IR AL HOME</Button>
-      </Link>
+      <Row type="flex" justify="center" align="top">
+        <Col span={2}>
+          <Link to="/home">
+            <Button>IR AL HOME</Button>
+          </Link>
+        </Col>
+      </Row>
 
       <Divider>CREANDO PERSONAJE</Divider>
 
-      <Form
-        name="basic"
-        labelCol={{ span: 5 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="on"
-        form={form}
-        //   style={{ width: 1200 }}
-      >
-        <Form.Item
-          label="Name:"
-          name="name"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input style={{ width: 300 }} />
-        </Form.Item>
+      <Row justify="center" align="top" type="flex">
+        <Col className="gutter-row" span={8}>
+          <Form
+            name="basic"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="on"
+            form={form}
+            //   style={{ width: 1200 }}
+          >
+            <div className="gutter-box">
+              <Form.Item
+                label="Name:"
+                name="name"
+                rules={[
+                  { required: true, message: "Please input your username!" },
+                ]}
+              >
+                <Input style={{ width: 300 }} />
+              </Form.Item>
+            </div>
 
-        <Form.Item
-          label="Last Name:"
-          name="last_name"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input style={{ width: 300 }} />
-        </Form.Item>
+            <Form.Item
+              label="Last Name:"
+              name="last_name"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input style={{ width: 300 }} />
+            </Form.Item>
 
-        <Form.Item
-          label="Type Document:"
-          name="type_document"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Select style={{ width: 100 }}>
-            <Select.Option value="T.I">T.I</Select.Option>
-            <Select.Option value="C.C.">C.C</Select.Option>
-            <Select.Option value="DNI">DNI</Select.Option>
-            <Select.Option value="OTRO">Otro</Select.Option>
-          </Select>
-        </Form.Item>
+            <Form.Item
+              label="Document"
+              name="document"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input style={{ width: 300 }} />
+            </Form.Item>
 
-        <Form.Item
-          label="Document"
-          name="document"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input style={{ width: 300 }} />
-        </Form.Item>
+            <div className="gutter-box-item">
+              <Form.Item
+                label="Type Document:"
+                name="type_document"
+                rules={[
+                  { required: true, message: "Please input your username!" },
+                ]}
+              >
+                <Select style={{ width: 100 }}>
+                  <Select.Option value="T.I">T.I</Select.Option>
+                  <Select.Option value="C.C.">C.C</Select.Option>
+                  <Select.Option value="DNI">DNI</Select.Option>
+                  <Select.Option value="OTRO">Otro</Select.Option>
+                </Select>
+              </Form.Item>
+            </div>
 
-        <Form.Item
-          label="Hobbie:"
-          name="hobbie"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Select style={{ width: 100 }}>
-            <Select.Option value="Correr">Correr</Select.Option>
-            <Select.Option value="Bailar">Bailar</Select.Option>
-            <Select.Option value="Cantar">Cantar</Select.Option>
-            <Select.Option value="Otro">Otro</Select.Option>
-          </Select>
-        </Form.Item>
+            <Form.Item
+              label="Hobbie:"
+              name="hobbie"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Select style={{ width: 100 }}>
+                <Select.Option value="Correr">Correr</Select.Option>
+                <Select.Option value="Bailar">Bailar</Select.Option>
+                <Select.Option value="Cantar">Cantar</Select.Option>
+                <Select.Option value="Otro">Otro</Select.Option>
+              </Select>
+            </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+            {/* <Row
+              type="flex"
+              justify="space-between"
+              align="top"
+              gutter={60}
+            ></Row> */}
+
+            <Form.Item wrapperCol={{ offset: 9, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 }

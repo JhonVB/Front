36074e524583c2bbae73e48c7212d9a741213213
JhonVB <jhonba2020@gmail.com>
2022-07-22 00:45:06 -0500

    app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Page/Home/Home";
 import Formulario from "./Page/Formulario/Formulario";
 import Login from "./Page/Login/Login";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home />} exact />
           <Route path="/create" element={<Formulario />} />
-          <Route path="/" element={<Login />} Login />
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>

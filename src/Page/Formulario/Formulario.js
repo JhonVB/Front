7926c517c64014d0f0f6f0f6d035246036fa71e5
JhonVB <jhonba2020@gmail.com>
2022-07-21 20:52:29 -0500

    Centrado de formulario y control de documento

--- src/Page/Formulario/Formulario.js
+++ src/Page/Formulario/Formulario.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { Button, Form, Input, Select, Divider, Row, Col } from "antd";
+import { Link } from "react-router-dom";
+import { Button, Form, Input, Select, Divider, Row, Col, message } from "antd";
 import { createPersona } from "../../Redux/actions";
-import "./Formulario.css";
 import "antd/dist/antd.css";
 
 function Formulario() {
   const [form] = Form.useForm();
   const dispatch = useDispatch();
-  const navegador = useNavigate();
 
   const errores = useSelector((state) => state.errors);
 
@@ -39,17 +37,19 @@ function Formulario() {
   return (
     <div className="contendedor">
       <Row type="flex" justify="center" align="top">
-        <Col span={2}>
+        <Col span={3.5} style={{ margin: 18 }}>
           <Link to="/home">
-            <Button>IR AL HOME</Button>
+            <Button type="primary" shape="round" style={{ width: 200 }}>
+              Go to home!
+            </Button>
           </Link>
         </Col>
       </Row>
 
-      <Divider>CREANDO PERSONAJE</Divider>
+      <Divider style={{ color: "blue" }}>CREANDO PERSONAJE</Divider>
 
       <Row justify="center" align="top" type="flex">
-        <Col className="gutter-row" span={8}>
+        <Col span={9}>
           <Form
             name="basic"
             labelCol={{ span: 5 }}
@@ -59,25 +59,20 @@ function Formulario() {
             onFinishFailed={onFinishFailed}
             autoComplete="on"
             form={form}
-            //   style={{ width: 1200 }}
           >
-            <div className="gutter-box">
-              <Form.Item
-                label="Name:"
-                name="name"
-                rules={[
-                  { required: true, message: "Please input your username!" },
-                ]}
-              >
-                <Input style={{ width: 300 }} />
-              </Form.Item>
-            </div>
+            <Form.Item
+              label="Name:"
+              name="name"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
+              <Input style={{ width: 300 }} />
+            </Form.Item>
 
             <Form.Item
               label="Last Name:"
               name="last_name"
               rules={[
-                { required: true, message: "Please input your username!" },
+                { required: true, message: "Please input your last name!" },
               ]}
             >
               <Input style={{ width: 300 }} />
@@ -87,35 +82,35 @@ function Formulario() {
               label="Document"
               name="document"
               rules={[
-                { required: true, message: "Please input your username!" },
+                { required: true, message: "Please input your document!" },
+                {
+                  pattern: RegExp(/^\d{1,100}$/g),
+                  message: "Solo se permiten numeros enteros",
+                },
               ]}
             >
               <Input style={{ width: 300 }} />
             </Form.Item>
 
-            <div className="gutter-box-item">
-              <Form.Item
-                label="Type Document:"
-                name="type_document"
-                rules={[
-                  { required: true, message: "Please input your username!" },
-                ]}
-              >
-                <Select style={{ width: 100 }}>
-                  <Select.Option value="T.I">T.I</Select.Option>
-                  <Select.Option value="C.C.">C.C</Select.Option>
-                  <Select.Option value="DNI">DNI</Select.Option>
-                  <Select.Option value="OTRO">Otro</Select.Option>
-                </Select>
-              </Form.Item>
-            </div>
+            <Form.Item
+              label="Type Document:"
+              name="type_document"
+              rules={[
+                { required: true, message: "Please input your type document!" },
+              ]}
+            >
+              <Select style={{ width: 100 }}>
+                <Select.Option value="T.I">T.I</Select.Option>
+                <Select.Option value="C.C.">C.C</Select.Option>
+                <Select.Option value="DNI">DNI</Select.Option>
+                <Select.Option value="OTRO">Otro</Select.Option>
+              </Select>
+            </Form.Item>
 
             <Form.Item
               label="Hobbie:"
               name="hobbie"
-              rules={[
-                { required: true, message: "Please input your username!" },
-              ]}
+              rules={[{ required: true, message: "Please input your hobbie!" }]}
             >
               <Select style={{ width: 100 }}>
                 <Select.Option value="Correr">Correr</Select.Option>
@@ -124,13 +119,6 @@ function Formulario() {
                 <Select.Option value="Otro">Otro</Select.Option>
               </Select>
             </Form.Item>
-
-            {/* <Row
-              type="flex"
-              justify="space-between"
-              align="top"
-              gutter={60}
-            ></Row> */}
 
             <Form.Item wrapperCol={{ offset: 9, span: 16 }}>
               <Button type="primary" htmlType="submit">

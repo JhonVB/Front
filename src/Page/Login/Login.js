93f53aa0aa375cd1ae951285a9349a95767f87e3
JhonVB <jhonba2020@gmail.com>
2022-07-21 20:51:22 -0500

    Base de login

--- src/Page/Login/Login.js
+++ src/Page/Login/Login.js
@@ -1,14 +1,54 @@
 import React, { useEffect, useState } from "react";
+import { Button, Checkbox, Form, Input } from "antd";
 import { useDispatch } from "react-redux";
-import { getPersonas } from "../../Redux/actions";
+import { logear } from "../../Redux/actions";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const dispatch = useDispatch();
+
+  const onFinish = (values) => {
+    console.log(values);
+    dispatch(logear(values));
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <div>
-      <Link to="/home">
-        <button>Logeate</button>
-      </Link>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
